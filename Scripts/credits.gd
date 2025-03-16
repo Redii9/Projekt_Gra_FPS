2f8ee98a194ae852941e04feba1a60beeca66650
Adrian Rewera <adrian.rewera9@gmail.com>
2025-03-16 17:50:32 +0100

    Dodanie credits
    - Dodanie funcjonalności przełączenia na credits
    - Dodanie panelu z credits oraz wstępne wypisanie autorów z możliwością użycia linków
    - Dodanie przycisku return w credits

---
+++ Scripts/credits.gd
@@ -0,0 +1,32 @@
+extends Control
+
+var main_menu: Node = null
+
+func _ready() -> void:
+	$Control/VBoxContainer/adrian_credits.text = "[url=https://redii9.itch.io]Adrian Rewera(Redii9)[/url]"
+	$Control/VBoxContainer/karol_credits.text = "[url=]Karol Ptak[/url]"
+	$Control/VBoxContainer/michal_credits.text = "[url=]Michał Podsiadło[/url]"
+	$Control/VBoxContainer/kamil_credits.text = "[url=]Kamil Raczyński[/url]"
+
+
+func _on_return_pressed() -> void:
+	# Usuniecie wszystkich instancji w grupie
+	for credits in get_tree().get_nodes_in_group("credits_instances"):
+		credits.queue_free()
+	
+	if main_menu:
+		main_menu.is_credits_open = false
+
+func _on_redii_credits_meta_clicked(meta: Variant) -> void:
+	OS.shell_open(meta)
+
+func _on_karol_credits_meta_clicked(_meta: Variant) -> void:
+	pass # Replace with function body.
+
+
+func _on_michal_credits_meta_clicked(_meta: Variant) -> void:
+	pass # Replace with function body.
+
+
+func _on_kamil_credits_meta_clicked(_meta: Variant) -> void:
+	pass # Replace with function body.
--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -1,5 +1,6 @@
 extends Control
 
+var is_credits_open: bool = false
 
 func _on_start_game_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/main.tscn")
@@ -10,8 +11,16 @@ func _on_exit_pressed() -> void:
 
 
 func _on_options_pressed() -> void:
-	pass # Replace with function body.
+	pass
 
 
 func _on_credits_pressed() -> void:
-	pass # Replace with function body.
+	if is_credits_open:
+		return 
+	
+	is_credits_open = true
+	 
+	var credits = load("res://Scenes/credits.tscn").instantiate()
+	credits.add_to_group("credits_instances")
+	credits.main_menu = self # Przekazanie referencji do main_menu
+	get_tree().root.add_child(credits)
