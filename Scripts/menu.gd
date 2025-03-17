2b435072b2791604075b5846e59a480ac5b84db2
Adrian Rewera <adrian.rewera9@gmail.com>
2025-03-17 19:39:25 +0100

    Zmiana w opcjach
    - Dodanie opcji fullscreen z zapisem do pliku

--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -3,6 +3,15 @@ extends Control
 var is_credits_open: bool = false
 var is_options_open: bool = false
 
+var config = ConfigFile.new()
+
+func _ready() -> void:
+	config.load("user://settings.cfg")
+	load_fullscreen()
+	# sciezka do katalogu testowanie:
+	print("Ścieżka do katalogu user://: ", OS.get_user_data_dir())
+	OS.shell_open(OS.get_user_data_dir())
+
 func _on_start_game_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/main.tscn")
 
@@ -33,3 +42,10 @@ func _on_credits_pressed() -> void:
 	credits.add_to_group("credits_instances")
 	credits.menu = self # Przekazanie referencji do menu
 	get_tree().root.add_child(credits)
+
+func load_fullscreen() -> void:
+	var fullscreen = config.get_value("video", "fullscreen", false)
+	if fullscreen:
+		get_window().mode = Window.MODE_FULLSCREEN
+	else:
+		get_window().mode = Window.MODE_WINDOWED
--- Scripts/options.gd
+++ Scripts/options.gd
@@ -1,7 +1,13 @@
 extends Control
 
 var menu: Node = null
+var config = ConfigFile.new()
 
+@export var fullscreen_button: CheckButton
+
+func _ready() -> void:
+	config.load("user://settings.cfg")
+	load_fullscreen()
 
 func _on_return_pressed() -> void:
 	# Usuniecie wszystkich instancji w grupie
@@ -10,3 +16,19 @@ func _on_return_pressed() -> void:
 	
 	if menu:
 		menu.is_options_open = false
+
+func load_fullscreen() -> void:
+	var fullscreen = config.get_value("video", "fullscreen", false)
+	if fullscreen:
+		get_window().mode = Window.MODE_FULLSCREEN
+	else:
+		get_window().mode = Window.MODE_WINDOWED
+	fullscreen_button.button_pressed = fullscreen
+
+func _on_fullscreen_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		get_window().mode = Window.MODE_FULLSCREEN
+	else:
+		get_window().mode = Window.MODE_WINDOWED
+	config.set_value("video", "fullscreen", toggled_on)
+	config.save("user://settings.cfg")
