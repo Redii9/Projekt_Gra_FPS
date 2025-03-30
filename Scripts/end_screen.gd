3e363813edb6bb68fc42042ae5db2543a53b9303
Adrian Rewera <adrian.rewera9@gmail.com>
2025-03-30 16:27:10 +0200

    Dodano przycisk retry

--- Scripts/end_screen.gd
+++ Scripts/end_screen.gd
@@ -5,3 +5,10 @@ func _on_menu_pressed() -> void:
 		end_screen.queue_free()
 	
 	get_tree().change_scene_to_file("res://Scenes/menu.tscn")
+
+
+func _on_retry_pressed() -> void:
+	for end_screen in get_tree().get_nodes_in_group("end_screen_instances"):
+		end_screen.queue_free()
+	
+	get_tree().change_scene_to_file("res://Scenes/main.tscn")
