65e75011110ab4361823191f571eed86c26b58aa
Adrian Rewera <adrian.rewera9@gmail.com>
2025-04-09 20:50:04 +0200

    Dodano Area3D
    - Dodano Area3D do markerów, który sprawdza czy nie ma przeciwników w obszarze respawnu
    - Blokuje możliwość respawnu wroga w miejscu gdzie znajduje się już wróg

--- Scripts/respawn_enemy.gd
+++ Scripts/respawn_enemy.gd
@@ -8,11 +8,26 @@ load("res://Scenes/enemy_fast.tscn"), load("res://Scenes/enemy_range.tscn")]
 func spawn_enemy() -> void:
 	var alive_enemies = get_tree().get_nodes_in_group("enemy").size()
 	if alive_enemies < max_enemies:
+		var available_markers = []
+		for marker in spawn_markers:
+			var area = marker.get_node("Area3D")
+			if not is_enemies_in_area(area):
+				available_markers.append(marker)
+		
+		if available_markers.is_empty():
+			return
+		
 		var random_enemy = enemy_scene.pick_random()
-		var random_marker = spawn_markers.pick_random()
+		var random_marker = available_markers.pick_random()
 		var enemy = random_enemy.instantiate()
 		enemy.global_transform = random_marker.global_transform
 		get_parent().add_child(enemy)
 
 func _on_timer_timeout() -> void:
 	spawn_enemy()
+
+func is_enemies_in_area(area: Area3D) -> bool:
+	for body in area.get_overlapping_bodies():
+		if body.is_in_group("enemy"):
+			return true
+	return false
