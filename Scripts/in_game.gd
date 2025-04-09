b0e9b6b064b60fef7ebc2801e2f77916650b4dad
Adrian Rewera <adrian.rewera9@gmail.com>
2025-04-09 15:45:31 +0200

    Dodano hp na ekranie gry

--- Scripts/in_game.gd
+++ Scripts/in_game.gd
@@ -1,8 +1,11 @@
 extends Control
 
 var kill_count: int = 0
-@onready var kills: Label = $Label
+@onready var kills: Label = $VBoxContainer/Label
+@onready var player_health: Label = $VBoxContainer/Label2
+
+@onready var player: CharacterBody3D = get_tree().get_first_node_in_group("player")
 
 func _process(_delta: float) -> void:
 	kills.text = str(kill_count)
-	
+	player_health.text = str(player.health)
